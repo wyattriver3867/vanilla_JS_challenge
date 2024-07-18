f6a80a7a0207876235e4dcac0e5f6d4761036f08
wyattriver3867 <wyattriver3867@gmail.com>
2024-07-18 17:01:50 +0900

    3.5 finished

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
-function handleTitleClick() {
-  console.log("title was clicked!");
-  title.style.color = "blue";
+console.dir(h1);
+
+function handleH1Click() {
+  console.log("h1 was clicked!");
+  h1.style.color = "blue";
 }
 
-title.addEventListener("click", handleTitleClick);
+function handleMouseEnter() {
+  h1.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  h1.innerText = "Mouse is gone!";
+}
+
+h1.addEventListener("click", handleH1Click);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+window.addEventListener("resize", handleWindowResize);
