7c0149e666f9b1be63d0f63ee23763ca3bde524f
wyattriver3867 <wyattriver3867@gmail.com>
2024-07-19 17:53:39 +0900

    3.7 finished

--- 1_study/app.js
+++ 1_study/app.js
@@ -3,15 +3,12 @@ const h1 = document.querySelector(".hello h1");
 console.dir(h1);
 
 function handleH1Click() {
-  const currentColor = h1.style.color;
-  let newColor;
-  // let은 재할당이 가능하니까, 일단 값을 비워둠.
-  if (currentColor === "blue") {
-    newColor = "tomato";
+  const clickedClass = "active";
+  if (h1.className === clickedClass) {
+    h1.className = "";
   } else {
-    newColor = "blue";
+    h1.className = clickedClass;
   }
-  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleH1Click);
