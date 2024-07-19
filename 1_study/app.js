289da72b1606032c40db2a131ab0e1e8600dab22
wyattriver3867 <wyattriver3867@gmail.com>
2024-07-19 18:12:07 +0900

    3.8 finished

--- 1_study/app.js
+++ 1_study/app.js
@@ -3,12 +3,8 @@ const h1 = document.querySelector(".hello h1");
 console.dir(h1);
 
 function handleH1Click() {
-  const clickedClass = "active";
-  if (h1.className === clickedClass) {
-    h1.className = "";
-  } else {
-    h1.className = clickedClass;
-  }
+  h1.classList.toggle("active");
+  // "active"같은 string이 반복되면 따로 함수를 설정해도 됨.
 }
 
 h1.addEventListener("click", handleH1Click);
