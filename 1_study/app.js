7daa8d643e2145a1e1ced86132a13a67145ce00e
wyattriver3867 <wyattriver3867@gmail.com>
2024-07-22 18:06:32 +0900

    4.1 finished

--- 1_study/app.js
+++ 1_study/app.js
@@ -1,9 +1,6 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-  console.log(loginInput.value);
-  console.log("click");
-}
+function onLoginBtnClick() {}
 
 loginButton.addEventListener("click", onLoginBtnClick);
