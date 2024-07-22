5cfb6c67e291bfaec07aa79717b3f60eef4c6173
wyattriver3867 <wyattriver3867@gmail.com>
2024-07-22 20:14:03 +0900

    4.3 finished

--- 1_study/app.js
+++ 1_study/app.js
@@ -1,9 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
 }
 
-loginForm, addEventListener("submit", onLoginSubmit);
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+  // alert("clicked!");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
