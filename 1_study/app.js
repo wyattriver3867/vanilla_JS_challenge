08936864b2c5c14873e4e6c85e8b7d308d16a728
wyattriver3867 <wyattriver3867@gmail.com>
2024-07-23 17:09:38 +0900

    4.4 finished

--- 1_study/app.js
+++ 1_study/app.js
@@ -1,18 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
-}
-
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
-  // alert("clicked!");
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
