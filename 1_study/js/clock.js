825bfec226e61ab31349993762c56ea62a7a1b83
wyattriver3867 <wyattriver3867@gmail.com>
2024-07-24 19:35:37 +0900

    5.1 finished

--- 1_study/js/clock.js
+++ 1_study/js/clock.js
@@ -1,8 +1,10 @@
 const clock = document.querySelector("h2#clock");
 //h2의 id가 clock인걸 찾기 때문에 붙여서 씀.
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 5000);
+getClock();
+setInterval(getClock, 1000);
