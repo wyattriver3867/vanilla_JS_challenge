1e485bda0ed6e6c8b1fc1f330c99ce1b611eb067
wyattriver3867 <wyattriver3867@gmail.com>
2024-07-24 21:21:53 +0900

    5.2 finished

--- 1_study/js/clock.js
+++ 1_study/js/clock.js
@@ -3,7 +3,14 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  //padStart는 string에만 작동하므로, 나온 값을 String()으로 변환하고 붙여야 함.
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+
+  //더 간단한 방식
+  //clock.innerText = new Date().toLocaleTimeString("en-US", { hour12: false });
 }
 
 getClock();
